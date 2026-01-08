4a8b61312e178592bf20097564a1965430eb092d
Quang Tuấn <tuannq>
2026-01-08 16:48:29 +0700

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/billiard-zero-sum-tracker/sw.js', { scope: '/billiard-zero-sum-tracker/' })})}
